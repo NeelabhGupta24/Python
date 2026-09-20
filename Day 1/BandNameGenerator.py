48843dd344066ba5dbdc86ec8df5a6485de09b87
Neelabh Gupta <98702801+NeelabhGupta24@users.noreply.github.com>
2026-09-20 17:48:46 +0530

    Delete Day 1 directory

--- Day 1/BandNameGenerator.py
+++
@@ -1,4 +0,0 @@
-print("Welcome to the band name generator")
-city = input("Enter The name of the city you grew up in :")
-pet = input("What animal is your pet :")
-print(f"Your band could be named {city} {pet}s")
